b2e1d29adfb0347a95e8d50a6273c886f3e3f3fa
codesOfSakshi <sakshi.mishra@sjsu.edu>
2021-11-20 17:11:52 -0800

    enroll customer apis

--- Api/Airlines/urls.py
+++ Api/Airlines/urls.py
@@ -2,7 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('create/', views.create_flight ),
-    path('flights/', views.flight_details),
-    path('flight/', views.get_flight),
+    path('enroll', views.add_customer),
+
 ]

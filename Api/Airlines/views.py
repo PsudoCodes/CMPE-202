cbabab3cc64605570a7fed41f5f33c92ca2a25af
codesOfSakshi <sakshi.mishra@sjsu.edu>
2021-11-23 18:46:48 -0800

    book and search flight apis

--- Api/Airlines/views.py
+++ Api/Airlines/views.py
@@ -61,3 +61,64 @@ def login(request):
             return Response({"success": True})
         return Response({"success": False})
 
+
+@api_view(["GET"])
+def search_flights(request):
+    """
+    returns flights based on given filters
+    :param request:
+    :return:
+    """
+    if request.method == 'GET':
+        from_location = request.GET.get('from_location')
+        to_location = request.GET.get('to_location')
+        travellers = request.GET.get('travellers')
+        seat_type = request.GET.get('seat_type')
+        departure = datetime.fromisoformat(request.GET.get('start'))
+        arrival = datetime.fromisoformat(request.GET.get('drop'))
+
+        queried_flights = Flight.objects.filter(from_location=from_location, to_location=to_location,
+                                                departure=departure, arrival=arrival).exclude(available_seats=0)
+
+        return Response({"results": queried_flights.values()})
+
+
+@api_view(["POST"])
+def book_tickets(request):
+    """
+    books ticket without seat number
+    :param request:
+    :return:
+    """
+    if request.method == 'POST':
+        flight_number = request.data.get('flight_number')
+        customer_id = request.data.get('customer_id')
+        amount = request.data.get('amount')
+        name_on_card = request.data.get('name_on_card')
+        card_number = request.data.get('card_number')
+        expiry_date = datetime.fromisoformat(request.data.get('expiry_date'))
+        cvv = request.data.get('cvv')
+        queried_flight = Flight.objects.filter(flight_number=flight_number, available_seats__gt=0).last()
+        if queried_flight:
+            existing_booking = Booking.objects.filter(customer_id=customer_id, flight_id=queried_flight.id).exclude(
+                status=Booking.STATUS.cancelled
+            )
+            if existing_booking:
+                raise Exception("Already booking exists!")
+            else:
+                booking_reference_id = ''.join([random.choice(string.ascii_uppercase + string.digits) for _ in range(8)])
+                try:
+                    booking = Booking.objects.create(customer_id=customer_id, flight_id=queried_flight.id, amount=amount,
+                                                     status=Booking.STATUS.booked, card_number=card_number, cvv=cvv,
+                                                     expiry_date=expiry_date, name_on_card=name_on_card,
+                                                     booking_reference_id=booking_reference_id
+                                                     )
+                except Exception as e:
+                    return Response({"error": "Try again", "details": e})
+                queried_flight.available_seats = queried_flight.available_seats - 1
+                queried_flight.save()
+                return Response({"booking_details": model_to_dict(booking),
+                                 "customer_details": model_to_dict(booking.customer),
+                                 "flight_details": model_to_dict(booking.flight)}
+                                )
+

847660fa99501c4b194aa19d0b623310de82a1b0
Akshay Jain <akshay.jain@sjsu.edu>
2021-12-02 22:22:35 -0800

    updated reward system

--- UI/src/index.js
+++ UI/src/index.js
@@ -13,8 +13,7 @@ ReactDOM.render(
     <BrowserRouter>
       <App/>
     </BrowserRouter>,document.getElementById('root')
-     //<Checkout />
-  // </React.StrictMode>,
+     
 );
 
 // If you want to start measuring performance in your app, pass a function
